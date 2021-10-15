a5017221fdb1e2e18e9ec9f3e0735296c4fd6dcb
Rayhan Sheikh <rsheikh201031@mscse.uiu.ac.bd>
2021-10-16 01:23:29 +0600

    css added

--- Restaurant_management_system/settings.py
+++ Restaurant_management_system/settings.py
@@ -128,7 +128,7 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
